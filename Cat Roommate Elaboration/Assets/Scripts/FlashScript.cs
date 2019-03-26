0ab8d150d8e3e31103ab0af7c405b083ccbfc609
Adam Goren <amg1090@nyu.edu>
2019-03-26 05:16:42 -0400

    Finalized

---
+++ Cat Roommate Elaboration/Assets/Scripts/FlashScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashScript : MonoBehaviour
+{
+    public CanvasGroup myCG;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myCG.alpha = myCG.alpha - Time.deltaTime;
+        if (myCG.alpha <= 0)
+        {
+            myCG.alpha = 0;
+        }
+    }
+}
